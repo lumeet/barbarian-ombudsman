a46522852ad045bb240218c042177b465f021a55
Teemu <teemu@aikais.in>
2015-06-29 21:43:17 +0300

    Add Istanbul for coverage

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,14 @@
-var gulp   = require('gulp');
-var jshint = require('gulp-jshint');
-var jscs   = require('gulp-jscs');
-var mocha  = require('gulp-mocha');
+var gulp     = require('gulp');
+var jshint   = require('gulp-jshint');
+var jscs     = require('gulp-jscs');
+var mocha    = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 
 var jsPaths = ['./lib/**/*.js', './test/**/*.js', './bin/*.js', 'Gulpfile.js'];
+
+function handleError(err) {
+  console.log(err.toString());
+}
 
 gulp.task('jshint', function() {
   return gulp.src(jsPaths)
@@ -16,9 +21,20 @@ gulp.task('jscs', function() {
     .pipe(jscs());
 });
 
-gulp.task('mocha', function() {
-  return gulp.src('test/**/*_test.js', { read: false })
-    .pipe(mocha());
+gulp.task('mocha', function(callback) {
+  gulp.src('./lib/*.js')
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function() {
+      return gulp.src('./test/**/*_test.js', { read: false })
+        .pipe(mocha())
+        .pipe(istanbul.writeReports({
+          reporters: ['lcovonly', 'text', 'text-summary']
+        }))
+        .pipe(istanbul.enforceThresholds({ thresholds: { global: 40 } }))
+        .on('error', handleError)
+        .on('end', callback);
+    });
 });
 
 gulp.task('test', ['jshint', 'jscs', 'mocha']);
